752e61fe22fcf5f69865f2b69d9a2b9a20cd17ae
f-elix <guerinfelix08@gmail.com>
2025-04-17 14:32:31 -0400

    first commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,22 @@
+import { schema } from '$lib/schema';
+import { fail, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+export const load = async () => {
+	const form = await superValidate(zod(schema));
+
+	return { form };
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const formData = await request.formData();
+		const form = await superValidate(formData, zod(schema));
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		return { form };
+	}
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+
+	let { data } = $props();
+
+	const superform = superForm(data.form);
+
+	const { form, enhance, errors } = superform;
+</script>
+
+<form method="POST" use:enhance>
+	<label for="name">
+		<span>Name</span>
+		<input type="text" name="name" bind:value={$form.name} />
+		{#if $errors.name}
+			<p class="error">{$errors.name}</p>
+		{/if}
+	</label>
+	<label for="email">
+		<span>Email</span>
+		<input type="email" name="email" bind:value={$form.email} />
+		{#if $errors.email}
+			<p class="error">{$errors.email}</p>
+		{/if}
+	</label>
+	<label for="message">
+		<span>Message</span>
+		<textarea name="message" bind:value={$form.message}></textarea>
+		{#if $errors.message}
+			<p class="error">{$errors.message}</p>
+		{/if}
+	</label>
+	<button type="submit">Submit</button>
+</form>
+
+<style lang="postcss">
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 1rem;
+	}
+	label {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: 0.5rem;
+	}
+	.error {
+		color: red;
+	}
+</style>
